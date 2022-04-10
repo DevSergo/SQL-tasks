f2ad076a1efb6ecaa7c9b165250d468faaf4d86d
s-harito <60604804+s-harito@users.noreply.github.com>
2022-04-10 22:51:27 +0300

    Create Задача 1 (решение вариант 1).sql

---
+++ Задача 1 (решение вариант 1).sql
@@ -0,0 +1,16 @@
+SELECT
+	emp_name,
+	task_count
+	FROM
+		(
+			SELECT
+			e.emp_name,
+			(SELECT COUNT(t.id) FROM #employee e1
+			LEFT JOIN #tasks t ON t.assignee_id = e1.id
+			WHERE e1.id = e.id
+			GROUP BY e1.id
+			HAVING COUNT(t.id) < 3) task_count
+			FROM #employee e
+		) e2
+	WHERE
+		task_count IS NOT NULL
