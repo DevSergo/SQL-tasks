92b49ce9f360b711b0240ab24b61b61ce79ce36f
s-harito <60604804+s-harito@users.noreply.github.com>
2022-04-10 23:03:16 +0300

    Create Задача 1 (решение вариант 2).sql

---
+++ Задача 1 (решение вариант 2).sql
@@ -0,0 +1,13 @@
+-- ВАРИАНТ 2
+SELECT 
+	e.emp_name, 
+	t.cnt 
+	FROM #employee e
+	LEFT JOIN
+		(
+			SELECT COUNT(id) cnt, assignee_id FROM #tasks
+			GROUP BY assignee_id
+			HAVING COUNT(id) >= 3
+		) t
+	ON e.id = t.assignee_id
+	WHERE t.assignee_id IS NULL
