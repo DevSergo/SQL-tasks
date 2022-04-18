70959e6ce161ceebce8c185364d6f190635b63e0
Sergey Kharitonov <60604804+DevSergo@users.noreply.github.com>
2022-04-18 17:30:51 +0300

    Update Задача 1 (решение вариант 1).sql

--- Задача 1 (решение вариант 1).sql
+++ Задача 1 (решение вариант 1).sql
@@ -5,11 +5,13 @@ SELECT
 		(
 			SELECT
 			e.emp_name,
-			(SELECT COUNT(t.id) FROM #employee e1
-			LEFT JOIN #tasks t ON t.assignee_id = e1.id
-			WHERE e1.id = e.id
-			GROUP BY e1.id
-			HAVING COUNT(t.id) < 3) task_count
+			(
+				SELECT COUNT(t.id) FROM #employee e1
+				LEFT JOIN #tasks t ON t.assignee_id = e1.id
+				WHERE e1.id = e.id
+				GROUP BY e1.id
+				HAVING COUNT(t.id) < 3
+			)       task_count
 			FROM #employee e
 		) e2
 	WHERE
