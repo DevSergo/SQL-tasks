eca52c0279b29fc0a57f635859b037ba515f7ac6
Sergey Kharitonov <60604804+DevSergo@users.noreply.github.com>
2022-04-18 18:04:03 +0300

    Update Задача 2 (решение).sql

--- Задача 2 (решение).sql
+++ Задача 2 (решение).sql
@@ -1,24 +1,24 @@
-			SELECT pi2.agr_id, pp2.sum_pp, pi2.sum_pi 
-			FROM	
-				(
-				SELECT pi1.agr_id, SUM(pi1.payment_sum) sum_pi FROM
-					(
-						SELECT DISTINCT pi.* FROM #payments_principal  pp
-						FULL OUTER JOIN #payments_interest pi ON pi.agr_id = pp.agr_id
-						WHERE (pp.agr_id IS NOT NULL) AND (pi.agr_id IS NOT NULL)
-					)       pi1
-				GROUP BY
-					pi1.agr_id
-				) pi2
-			LEFT JOIN
-				(
-				SELECT pp1.agr_id, SUM(pp1.payment_sum) sum_pp FROM
-					(
-						SELECT DISTINCT pp.* FROM #payments_principal  pp
-						FULL OUTER JOIN #payments_interest pi ON pp.agr_id = pi.agr_id
-						WHERE (pp.agr_id IS NOT NULL) AND (pi.agr_id IS NOT NULL)
-					)       pp1
-				GROUP BY
-					pp1.agr_id
-				) pp2
-			ON pi2.agr_id = pp2.agr_id
+SELECT pi2.agr_id, pp2.sum_pp, pi2.sum_pi 
+FROM	
+	(
+	SELECT pi1.agr_id, SUM(pi1.payment_sum) sum_pi FROM
+		(
+			SELECT DISTINCT pi.* FROM #payments_principal  pp
+			FULL OUTER JOIN #payments_interest pi ON pi.agr_id = pp.agr_id
+			WHERE (pp.agr_id IS NOT NULL) AND (pi.agr_id IS NOT NULL)
+		)       pi1
+	GROUP BY
+		pi1.agr_id
+	) pi2
+LEFT JOIN
+	(
+	SELECT pp1.agr_id, SUM(pp1.payment_sum) sum_pp FROM
+		(
+			SELECT DISTINCT pp.* FROM #payments_principal  pp
+			FULL OUTER JOIN #payments_interest pi ON pp.agr_id = pi.agr_id
+			WHERE (pp.agr_id IS NOT NULL) AND (pi.agr_id IS NOT NULL)
+		)       pp1
+	GROUP BY
+		pp1.agr_id
+	) pp2
+ON pi2.agr_id = pp2.agr_id
